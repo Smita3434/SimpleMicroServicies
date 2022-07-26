4f81ea4b3f3f3b9c0c4fac77b245183d105a61ed
Smita3434 <sayalig94@gmail.com>
2022-07-26 17:39:48 +0530

    Add project files.

---
+++ UserServices/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserServices.Entities;
+
+namespace UserServices.Data
+{
+        public class AppDbContext : DbContext
+        {
+            public DbSet<Users> Users { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=LAPTOP-6IR2U05V\SQLEXPRESS01;Database=ReinSolutionADO.Net];Integrated Security=True;");
+            }
+        }
+
+    
+}
